5c8cabf1dc2d34b013521ad4d405c7222d8db4db
SE IK <heski0709@gmail.com>
2023-06-24 20:38:12 +0900

    판매상품 리스트 약간 수정

--- JAVA/welpair/src/main/java/com/hielectro/welpair/main/controller/MainController.java
+++ JAVA/welpair/src/main/java/com/hielectro/welpair/main/controller/MainController.java
@@ -2,7 +2,6 @@ package com.hielectro.welpair.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
